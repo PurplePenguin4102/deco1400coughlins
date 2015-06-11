87c2f5ed00120004f0e8ddd75e71ef5bf7486477
Joseph Ray <joseph.h.ray@gmail.com>
2015-06-12 08:59:37 +1000

    made the code a bit better

--- Group assignment 2 - update/js/toowoomba.js
+++ Group assignment 2 - update/js/toowoomba.js
@@ -1,13 +1,17 @@
 //add the DOM listener
-window.addEventListener("load", listenToowoomba, false);
+window.addEventListener("load", listenDestination, false);
 
-function listenToowoomba() {
+function listenDestination() {
+	//grab the buttons
 	var toowoomba = document.getElementById("toowoomba");
+	var brisbane = document.getElementById("brisbane");
+
 	//if we live in toowoomba we need to change the daddr
-	toowoomba.onclick = changeDaddr;
+	toowoomba.onclick = daddrToowoomba;
+	brisbane.onclick = daddrBrisbane;
 }
 
-function changeDaddr(event) {
+function daddrToowoomba(event) {
 	//grab the hidden input daddr
 	var daddr = document.getElementById("daddr");
 	//change it to the toowoomba office
@@ -17,4 +21,15 @@ function changeDaddr(event) {
 	var directionForm = document.getElementById("directionForm");
 	directionForm.submit();
 
+}
+
+function daddrBrisbane(event) {
+	//grab the hidden input daddr
+	var daddr = document.getElementById("daddr");
+	//change it to the brisbane office
+	daddr.value = "10 market street, Brisbane";
+
+	//submit the form
+	var directionForm = document.getElementById("directionForm");
+	directionForm.submit();
 }
